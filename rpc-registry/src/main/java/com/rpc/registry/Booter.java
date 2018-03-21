beb2dfb2bd64dffd7196094d23e82d4a6645a53e
yin.huang <923430998@qq.com>
2018-03-21 18:20:49 +0800

    rpc-registry

---
+++ rpc-registry/src/main/java/com/rpc/registry/Booter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.rpc.registry;
+
+import com.rpc.common.EmbbedTomcat;
+
+/**
+ * 
+ * @author yin.huang
+ * @date 2018年3月20日 上午11:09:45
+ *
+ */
+public class Booter {
+
+  public static void main(String[] args) {
+
+    EmbbedTomcat.main(args);
+  }
+}
